5d7b5d95da6b48f8b8301c4fcbf9fbe356878406
SHANU SHAHBIN V <shanushahbin11@gmail.com>
2024-09-27 22:50:46 +0530

    updated code

--- Social_Auth/Social_Auth/settings.py
+++ Social_Auth/Social_Auth/settings.py
@@ -142,3 +142,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SOCIALACCOUNT_LOGIN_ON_GET = True
